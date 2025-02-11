85747d4e82c39476dcacb1cc143539f49d13e949
RubyMythiliM <104880562+RubyMythiliM@users.noreply.github.com>
2025-02-11 21:29:33 +0530

    Update setup-vm.sh

--- deployment-scripts/setup-vm.sh
+++ deployment-scripts/setup-vm.sh
@@ -5,7 +5,7 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y nodejs npm docker.io nginx git
 
 echo "Setting up microservice environment..."
-git clone https://github.com/your-username/microservice-deployment.git /opt/microservice-app
+git clone https://github.com/RubyMythiliM/microservice-deployment.git /opt/microservice-app
 cd /opt/microservice-app
 npm install
 
